5cc6ed3ed8075f63bdd61bb97ad5c5d97ef42c78
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-17 09:36:45 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -33,12 +33,11 @@ else:
     USERBOT_PREFIX = "."
     PHONE_NUMBER = "+916969696969"  # Need for Userbot
     SUDO_USERS_ID = [
-        4543744343,
-        543214651351,
+        5289852546
     ]  # Sudo users have full access to everything, don't trust anyone
-    LOG_GROUP_ID = -100125431255
-    GBAN_LOG_GROUP_ID = -100125431255
-    MESSAGE_DUMP_CHAT = -1001181696437
+    LOG_GROUP_ID = -1001658407031
+    GBAN_LOG_GROUP_ID = -1001658407031
+    MESSAGE_DUMP_CHAT = -1001658407031
     WELCOME_DELAY_KICK_SEC = 300
     MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
     ARQ_API_KEY = "Get this from @ARQRobot"
